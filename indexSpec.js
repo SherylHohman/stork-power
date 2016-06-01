bb33f079fd736a7e779ef2439991a9c853db8930
Sheryl Hohman <sherylhohman@yahoo.com>
2016-05-31 22:30:07 -0700

    test: create a (dummy) function to run mocha-chai test against

--- indexSpec.js
+++ indexSpec.js
@@ -1,11 +1,18 @@
 var chai = require('chai');
 var expect = require('chai').expect;
+var word = require('./index');
 
 describe('Sanitize', function(){
+  var inputWord = 'HELLO WORLD';
+  var outputWord = word.sanitize(inputWord);
+
   it ('returns lowercase of a string', function(){
-    expect('HELLO WORLD').to.equal('HELLO WORLD');
-  })
-  it ('removes any hyphen', function(){
-    expect('Hello-World').to.equal('Hello World');
-  })
+    expect(outputWord).to.equal('hello world');
+    expect(outputWord).to.not.equal('HELLO WORLD');
+    expect(outputWord).to.be.a('string');
+    expect(outputWord).to.not.be.a('number');
+    expect(outputWord).to.contain('hello');
+  });
+  it ('removes any hyphen')
+  // not running any test - should output 'pending'
 })
