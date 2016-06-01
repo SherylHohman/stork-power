ca40c411ffc785bcf51600c490d3602053144562
Sheryl Hohman <sherylhohman@yahoo.com>
2016-05-31 22:11:33 -0700

    devops: Mocha-Chai works: indexSpec.js updated with chai to create passing and failing tests

--- indexSpec.js
+++ indexSpec.js
@@ -1,4 +1,11 @@
+var chai = require('chai');
+var expect = require('chai').expect;
+
 describe('Sanitize', function(){
-  it ('returns lowercase of a string')
-  it ('removes any hyphen')
+  it ('returns lowercase of a string', function(){
+    expect('HELLO WORLD').to.equal('HELLO WORLD');
+  })
+  it ('removes any hyphen', function(){
+    expect('Hello-World').to.equal('Hello World');
+  })
 })
