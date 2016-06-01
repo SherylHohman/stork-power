f8055bbfb6df998a147e29401a75ef39406f9754
Sheryl Hohman <sherylhohman@yahoo.com>
2016-05-31 22:46:02 -0700

    test: update function, and write second test. Works.

--- indexSpec.js
+++ indexSpec.js
@@ -3,16 +3,21 @@ var expect = require('chai').expect;
 var word = require('./index');
 
 describe('Sanitize', function(){
-  var inputWord = 'HELLO WORLD';
-  var outputWord = word.sanitize(inputWord);
-
   it ('returns lowercase of a string', function(){
+    var inputWord = 'HELLO WORLD';
+    var outputWord = word.sanitize(inputWord);
+
     expect(outputWord).to.equal('hello world');
     expect(outputWord).to.not.equal('HELLO WORLD');
     expect(outputWord).to.be.a('string');
     expect(outputWord).to.not.be.a('number');
     expect(outputWord).to.contain('hello');
   });
-  it ('removes any hyphen')
-  // not running any test - should output 'pending'
+
+  it ('removes any hyphen', function(){
+    var inputWord = 'HELLO-WORLD';
+    var outputWord = word.sanitize(inputWord);
+
+    expect(outputWord).to.equal('hello world');
+  })
 })
